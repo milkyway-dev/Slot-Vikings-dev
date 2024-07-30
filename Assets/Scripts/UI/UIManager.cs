5430ba2bef8c3a78a93247ff42ac6d00e7475455
EkanshBatra <>
2024-07-30 14:49:35 +0530

    Jackpot amount update

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -424,7 +424,7 @@ public class UIManager : MonoBehaviour
             }
             if (paylines.symbols[i].Name.ToUpper() == "JACKPOT")
             {
-                if (Jackpot_Text) Jackpot_Text.text = "Jackpot: Mega win triggered by 5 Jackpot symbols on a pay line.\nPayout: <color=yellow>" + paylines.symbols[i].defaultAmount;
+                if (Jackpot_Text) Jackpot_Text.text = "Jackpot: Mega win triggered by 5 Jackpot symbols on a pay line.\nPayout: <color=yellow>" + paylines.symbols[i].defaultAmount + "x";
             }
         }
     }
